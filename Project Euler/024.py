61d1dfb2d51267d522326b206d010bb00bf4988b
pliu037 <pengliu12@gmail.com>
2015-01-24 23:26:04 -0500

    Project Euler
    Problem 24

--- Project Euler/024.py
+++ Project Euler/024.py
@@ -6,10 +6,27 @@ from gmpy2 import fac
 '''
 Finds and returns the n-th lexographic permutation of strng
 Method:
-
+Given a sorted list of length i, there are fac(i - 1) permutations possible without changing the first
+position. If n >= fac(i - 1), the first position must change. For every multiple of fac(i - 1), the
+first position changes once and consumes fac(i - 1) from n. To enforce lexographic ordering, when i
+changes, it must change to the next lowest element. As the list is sorted, this is equivalent to
+swapping it with the (n/fac(i - 1) + 1)-th position. After this iteration, the first position will be
+the correct value. Sort the remaining i - 1 positions and, using the reduced n, repeat the process for
+the sublist of length i - 1. After j iterations, the first j positions will be correct.
+Observation:
+Runs in O(i) time, where i is the number of digits, as opposed to the O(i!) time of the below solution.
 '''
 def myLexographicPermutation(strng, n):
+    n -= 1
     elements = sorted(strng)
+    for i in xrange(len(elements) - 1):
+        currentFac = fac(len(elements) - 1 - i)
+        if n >= currentFac:
+            temp = elements[i]
+            elements[i] = elements[n / currentFac + i]
+            elements[n / currentFac + i] = temp
+            n -= (n / currentFac)*currentFac
+            elements[i + 1:] = sorted(elements[i + 1:])
     return elements
     
 #Finds and returns the n-th lexographic permutation of strng
@@ -19,5 +36,5 @@ def lexographicPermutation(strng, n):
         iterator.next()
     return iterator.next()
     
-print lexographicPermutation('0123456789', 1000000)
-print myLexographicPermutation('0123456789', 1000000)
+print lexographicPermutation('000123456789', 100000000)
+print myLexographicPermutation('000123456789', 100000000)
