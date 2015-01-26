6314d59a0ab72edc7893b0bb210f6315e1876aac
pliu037 <pengliu12@gmail.com>
2015-01-26 09:34:39 -0500

    Project Euler and switched to IntelliJ
    Problem 31

--- Project Euler/031.py
+++ Project Euler/031.py
@@ -1,31 +1,60 @@
 #https://projecteuler.net/problem=31
 
 '''
+Recursively finds the number of combinations that obtain a target value, i, given the denominations
+specified in denomList
 '''
-def splitSmaller (array, n):
-    
-    return 0
+def recCombinations(denomList, values, i):
 
-'''
+    #Base case: If i = 0, the combination specified by this trace has attained n
+    if i == 0:
+        return 1
 
-Method:
-
-'''
-def combinations (n):
-    DENOMINATIONS = [200, 100, 50, 20, 10, 5, 2, 1]
-    
-    numCoins = []
-    numDenominations = len(DENOMINATIONS)
-    for i in xrange(numDenominations):
-        numCoins.append(n / DENOMINATIONS[i])
-        n = n / DENOMINATIONS[i]
-
-    combinations = 1
-        
-    for i in xrange(numDenominations):
-        while numCoins[i] > 0:
+    '''
+    Base case: If denomList only contains one more element, the combination specified by this trace
+    can only attain n if (i % <the final value in denomList> = 0)
+    Observation:
+    If denomList includes 1, this cannot occur.
+    '''
+    if len(denomList) == 1:
+        if (i % denomList[0] == 0):
+            return 1
+        else:
             return 0
 
+    j = 0
+    combinations = 0
+
+    '''
+    If denomList contains 2 or more elements and a target value, i, the number of viable
+    combinations given the trace up to this point is equal to the sum of the number of viable
+    combinations using 0 to j units of the largest denomination, leaving denomList[1:] and (i -
+    j*denomList[0]) as input into the recursive calls. The sum is returned.
+    Optimization: Using memoization, with l (the length of denomList; since denomList is sorted,
+    the length uniquely identifies the sublist) and i (here, i actually represents the argument to
+    be passed into the recursive call) as indices, if values[(l, i)] has not been calculated before,
+    calculate it, store it, and then add it to the sum. Otherwise, just add the memoized value to
+    the sum.
+    '''
+    while j*denomList[0] <= i:
+        if (len(denomList), i - j*denomList[0]) in values:
+            combinations += values[(len(denomList), i - j*denomList[0])]
+        else:
+            values[(len(denomList), i - j*denomList[0])] = recCombinations(denomList[1:],
+                values, i - j*denomList[0])
+            combinations += values[(len(denomList), i - j*denomList[0])]
+        j += 1
+
     return combinations
-    
-print combinations(200)
+
+'''
+Given a list of denominations and a target value, n, finds and returns the number of combinations
+that yield exactly n
+'''
+def combinations (denominations, n):
+    sortedDenom = sorted(denominations)
+    sortedDenom.reverse()
+    values = {}
+    return recCombinations(sortedDenom, values, n)
+
+print combinations([1, 2, 5, 10, 20, 50, 100, 200], 200)
