a14649e07b678eeec82c69d2297945a2568c0e65
pliu037 <pengliu12@gmail.com>
2015-01-30 02:05:42 -0500

    Project Euler
    Problem 36

---
+++ Project Euler/036.py
@@ -0,0 +1,48 @@
+#https://projecteuler.net/problem=36
+
+'''
+Generates <digits>-length palindromes in lexographic order
+Method:
+
+'''
+class PalindromeGenerator:
+    def __init__(self, digits):
+        self.digits = digits
+        self.odd = (digits % 2 != 0)
+        self.current = 10**((digits + 1)/2 - 1) - 1
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        self.current += 1
+        if self.current >= 10**((self.digits + 1)/2):
+            raise StopIteration()
+        digitArray = ['0' for _i in xrange(self.digits)]
+        base = str(self.current)
+        for i in xrange(len(base)):
+            digitArray[i] = base[i]
+            digitArray[self.digits - 1 - i] = base[i]
+        return int(''.join(i for i in digitArray))
+
+#Given a string, strng, returns True if it is a palindrome
+def isPalindrome(strng):
+    for i in xrange((len(strng) + 1)/2):
+        if strng[i] != strng[len(strng) - 1 - i]:
+            return False
+    return True
+
+'''
+Finds and returns the sum of all numbers with up to n digits, inclusive, that are palindromes in
+base 10 and base 2
+'''
+def sumPalindromes(n):
+    currentSum = 0
+    for digits in xrange(1, n + 1):
+        palindromeGen = PalindromeGenerator(digits)
+        for check in palindromeGen:
+            if isPalindrome(bin(check)[2:]):
+                currentSum += check
+    return currentSum
+
+print sumPalindromes(6)
--- Project Euler/081-082-083.py
+++ Project Euler/081-082-083.py
@@ -13,19 +13,17 @@ class Node:
         self.value = value
         self.paths = paths
 
-class Path:
-
-    def __init__(self, sum, lastNode)
-        self.sum = 0
-        self.lastNode = lastNode
-
 def findMinPath(map, start, stop):
     pq = []
     visited = set()
     heapq.heappush(start)
     while heapq:
         return 0
-    return 0
 
-
-
+def processData(array, directions):
+    map = []
+    for i in xrange(len(array)):
+        map.append([])
+        for j in xrange(len(array[i])):
+            map[i].append(Node(array[i][j], ))
+    return map
