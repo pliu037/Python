91f999779260cbdd72ac2bdd5c343a2cd0107aec
pliu037 <pengliu12@gmail.com>
2015-02-04 13:00:14 -0500

    Project Euler
    Problem 39

--- Project Euler/039.py
+++ Project Euler/039.py
@@ -1,26 +1,31 @@
 #https://projecteuler.net/problem=39
 
 from gmpy2 import is_square
-from gmpy2 import isqrt
+from math import sqrt
 
 '''
 Finds and returns the value of p for which a + b + c = p has the most Pythagorean triplets (a**2 +
 b**2 = c**2) for all p <= n
 Method:
-
+a + b + c <= n. By the triangle inequality, (a + b >= c) <=> (a + b + c >= 2c) <=> (n >= a + b + c
+>= 2c) <=> (n >= 2c) <=> (c <= n/2). If b > a, (a**2 + b**2 = c**2) <=> (2b**2 > c**2) <=> (b >
+c/sqrt(2)) and (2a**2 < c**2) <=> (a < c/sqrt(2)). If b < a, b < c/sqrt(2) and a > c/sqrt(2). Thus,
+for every b below c/sqrt(2), there is an a above c/sqrt(2) and vice versa. Since a and b are
+interchangeable, to avoid double-counting, b is only checked up to c/sqrt(2).
+Observation:
+Using an algorithm that generates only and all primitive Pythagorean triplets may result in faster
+running time.
 '''
 def findMaxPythagoreanTriplets(n):
     counts = [0 for _i in xrange(n + 2)]
-    x = 1
-    while x < n/3:
+    c = 5
+    while c < n/2:
         b = 1
-        while x + 2*b < n/2:
-            if is_square(x + 2*b):
-                print b + x + b + isqrt(x*(x + 2*b))
+        while (b < c / sqrt(2)):
+            if is_square(c**2 - b**2) and sqrt(c**2 - b**2) + b + c <= n:
+                counts[int(sqrt(c**2 - b**2)) + b + c] += 1
             b += 1
-        x += 1
-        while not is_square(x):
-            x += 1
+        c += 1
     return counts.index(max(counts))
 
-print findMaxPythagoreanTriplets(100)
+print findMaxPythagoreanTriplets(1000)
