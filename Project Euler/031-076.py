fca760b786250109e6465731d1b7b2759d5ff8c0
pliu037 <pengliu12@gmail.com>
2015-02-05 05:02:59 -0500

    Project Euler
    Problem 76

--- Project Euler/031-076.py
+++ Project Euler/031-076.py
@@ -1,4 +1,7 @@
-#https://projecteuler.net/problem=31
+'''
+https://projecteuler.net/problem=31
+https://projecteuler.net/problem=76
+'''
 
 '''
 Recursively finds the number of combinations that obtain a target value, i, given the denominations
@@ -58,3 +61,4 @@ def combinations (denominations, n):
     return recCombinations(sortedDenom, values, n)
 
 print combinations([1, 2, 5, 10, 20, 50, 100, 200], 200)
+print combinations([i for i in xrange(1, 100)], 100)
