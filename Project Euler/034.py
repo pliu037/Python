5f8e357235d8010ab5b8c6a5812161c4fd5ad77c
pliu037 <pengliu12@gmail.com>
2015-03-30 02:46:29 -0400

    Project Euler
    Optimized problem 34

--- Project Euler/034.py
+++ Project Euler/034.py
@@ -2,26 +2,67 @@
 
 from gmpy2 import fac
 from tools import findSumFacDigits
+from tools import isPermutation
+from itertools import combinations_with_replacement
+
+'''
+Finds and returns the maximum number of digits of an integer whose sum of the factorial of its digits
+is equal to itself
+Method:
+Each digit added increases the number by up to a factor of 10 (10**i where i is the number of digits)
+but adds up to 9! to the sum. Find i such that i*9! < 10**i.
+'''
+def findMaxDigits():
+    check = fac(9)
+    digits = 1
+    while check >= 10**digits:
+        check += fac(9)
+        digits += 1
+    return digits
 
 '''
 Finds and returns the sum of integers whose sum of the factorial of their digits is equal to that
 integer
 Method:
-Each digit added increases the number by up to a factor of 10 (10**i where i is the number of digits)
-but adds up to 9! to the sum. Find i such that i*9! < 10**i. Only need to check up to i*9! as it is
-the highest attainable sum given i digits.
+Generates combinations of up to <digits> digits chosen from 1-9 with replacement and finds the sum
+of their factorials. Since any permutation of the combination yields the same sum, if the sum is a
+permutation of the combination, the sum is an integer whose sum of the factorial of its digits is
+equal to itself. While the number of digits in the combination being checked is less than the number
+of digits in the sum, adding 0s to the combination being checked may lead to the sum becoming a
+permutation of the digits (adding digits chosen from 1-9 is covered by the combination generator).
+For each added 0, the sum to check is increased by 0! = 1.
 '''
-def findSumFactorials():
-    currentSum = fac(9)
-    digits = 1
-    while currentSum >= 10**digits:
-        currentSum += fac(9)
-        digits += 1
+def findSumFactorials(digits):
+    currentSum = 0
+    digitChars = "123456789"
+    for numDigits in xrange(1, digits + 1):
+        combos = combinations_with_replacement(digitChars, numDigits)
+        for combo in combos:
+            check = "".join(combo)
+            facSum = findSumFacDigits(int(check))
+            while True:
+                if isPermutation(int(check), facSum):
+                    currentSum += facSum
+                    break
+                check += '0'
+                facSum += 1
+                if len(check) > len(str(facSum)):
+                    break
+    return currentSum - 3
 
+'''
+Finds and returns the sum of integers whose sum of the factorial of their digits is equal to that
+integer
+Method:
+Only need to check up to <digits>*9! as it is the highest attainable sum given <digits> digits.
+'''
+def findSumFactorialsBrute(digits):
     currentSum = 0
     for i in xrange(3, digits*fac(9)):
         if (i == findSumFacDigits(i)):
             currentSum += i
     return currentSum
 
-print findSumFactorials()
+digits = findMaxDigits()
+print findSumFactorials(digits)
+print findSumFactorialsBrute(digits)
--- Project Euler/038.py
+++ Project Euler/038.py
@@ -3,29 +3,35 @@
 from itertools import permutations
 
 '''
-Determines whether the given 1-9 pandigital can be written as the concatenation of 1*x, 2*x... n*x
-where n > 1
-Method:
-It is given that 918273645 is a 1-9 pandigital that satisfies these conditions, placing a lower bound
-on the maximal solution. Any maximal solution must start with 9. Given this constraint, 
+Determines whether <num> can be written as the concatenation of 1*x, 2*x... n*x where n > 1 and x is
+the number formed by the first i digits of <num>
 '''
-def isPandigitalMultiple(n):
-    position = 4
+def isMultipleConcat(num, i):
+    position = i
     multiplier = 2
-    first = int("".join(n[:4]))
+    first = int("".join(num[:i]))
     while True:
         check = str(first*multiplier)
         for j in xrange(len(check)):
-            if position >= len(n) or check[j] != n[position]:
+            if position >= len(num) or check[j] != num[position]:
                 return False
             position += 1
-        if position == len(n):
+        if position == len(num):
             return True
         multiplier += 1
 
 '''
 Finds and returns the largest 1-9 pandigital that can be written as the concatenation of 1*x, 2*x...
 n*x where n > 1
+Method:
+Since it is given that 918273645 is a 1-9 pandigital that satisfies these conditions, any maximal
+solution must start with 9. Combined with the fact that each sequence starts with 1*x, x must start
+with 9. Any i-digit number starting with 9 multiplied by n <= 10 contains i + 1 digits. Since
+918273645 is the unique sequence resulting from a 1-digit x, alternative solutions must have i > 1
+digits. For i = 2, 3, 4, or 5+, the number of digits in subsequent terms is 3, 4, 5, or 6+,
+respectively. 2 + 3*(n - 1) = 9, 3 + 4*(n - 1) = 9, and 5 + 6*(n - 1) = 9 (the length of a 1-9
+pandigital) has no solution for integer values of n such that 1 < n <= 10 whereas 4 + 5*(n - 1) = 9
+does have a solution, indicating that x must be 4 digits long.
 '''
 def largestPandigitalMultiple():
 
@@ -34,7 +40,7 @@ def largestPandigitalMultiple():
     perms = permutations(digits)
 
     for perm in perms:
-        if isPandigitalMultiple(perm):
+        if isMultipleConcat(perm, 4):
             return "".join(perm)
 
 print largestPandigitalMultiple()
