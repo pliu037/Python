43dae998cab4acc9c2cb9084594b52d339b743a4
pliu037 <pengliu12@gmail.com>
2015-08-03 01:18:11 -0400

    Project Euler
    Problem 98

---
+++ Project Euler/054.py
@@ -0,0 +1,10 @@
+#https://projecteuler.net/problem=54
+
+def parseHands(line):
+    return None
+
+def getHand(hand):
+    return None
+
+def numWins():
+    return None
---
+++ Project Euler/098.py
@@ -0,0 +1,102 @@
+#https://projecteuler.net/problem=98
+
+from tools import getWordData
+from math import sqrt
+from itertools import combinations
+
+'''
+Given a list of some type (e.g.: String, Integer, etc.) and a function that maps an instance of that
+type into a tuple of its composites (e.g.: characters and digits, respectively), returns a hash of
+
+'''
+def getAnagrams(list, keyFunc):
+    anagrams = {}
+    for element in list:
+        check = keyFunc(element)
+        if check not in anagrams:
+            anagrams[check] = [element]
+        else:
+            anagrams[check].append(element)
+    toRemove = []
+    for key in anagrams:
+        if len(anagrams[key]) < 2:
+            toRemove.append(key)
+    for remove in toRemove:
+        anagrams.pop(remove)
+    return anagrams
+
+#
+def getWordKey(word):
+    return tuple(sorted(word))
+
+#
+def getNumberKey(n):
+    return tuple(sorted(str(n)))
+
+#
+def getSymbolKey(x):
+    dic = {}
+    for y in x:
+        if y in dic:
+            dic[y] += 1
+        else:
+            dic[y] = 1
+    return tuple(sorted(dic.values()))
+
+#
+def checkMatch(wordPair, numberPair, maxValue):
+    mapping = {}
+    for i in xrange(len(wordPair[0])):
+        mapping[wordPair[0][i]] = str(numberPair[0])[i]
+    for i in xrange(len(wordPair[0])):
+        if str(numberPair[1])[i] != mapping[wordPair[1][i]]:
+            return 0
+    return max(numberPair)
+
+#
+def checkAnagrams(wordAnagrams, numberAnagrams, maxValue):
+    for wordPair in combinations(wordAnagrams, 2):
+        for numbers in numberAnagrams:
+            if max(numbers) > maxValue:
+                for numberPair in combinations(numbers, 2):
+                    value = checkMatch(wordPair, numberPair, maxValue)
+                    if value > maxValue:
+                        maxValue = value
+    return maxValue
+
+'''
+
+'''
+def findLargestSqAnagram():
+    words = getWordData()
+    wordAnagrams = getAnagrams(words, getWordKey)
+    maxLength = 0
+    minLength = len(wordAnagrams.keys()[0])
+    for key in wordAnagrams:
+        if len(key) > maxLength:
+            maxLength = len(key)
+        if len(key) < minLength:
+            minLength = len(key)
+    squares = []
+    i = int(sqrt(10**minLength))
+    while i**2 < 10**maxLength:
+        squares.append(i**2)
+        i += 1
+    sqAnagrams = getAnagrams(squares, getNumberKey)
+    symbolAnagrams = {}
+    for key in sqAnagrams:
+        check = getSymbolKey(key)
+        if check in symbolAnagrams:
+            symbolAnagrams[check].append(sqAnagrams[key])
+        else:
+            symbolAnagrams[check] = [sqAnagrams[key]]
+    maxValue = 0
+    for key in wordAnagrams:
+        check = getSymbolKey(key)
+        if check in symbolAnagrams:
+            value = checkAnagrams(wordAnagrams[key], symbolAnagrams[check], maxValue)
+            if value > maxValue:
+                maxValue = value
+    return maxValue
+
+print findLargestSqAnagram()
