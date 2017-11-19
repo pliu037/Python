7fd8792181162d863e1ce759799ebbd3031380ca
Peng Liu <pengliu12@gmail.com>
2017-11-19 18:22:45 -0500

    IEEE Coding Challenge
    Added q3 implementation
    Started q3 tests

--- Miscellaneous/IEEE Coding Challenge/q3/q3.py
+++ Miscellaneous/IEEE Coding Challenge/q3/q3.py
@@ -1,2 +1,18 @@
-def find_string_multiplier_to_contain(outer_str, inner_str):
-    pass
+def find_number_of_repeats(substr, s):
+    if s == '':
+        return 0
+    for i in xrange(len(substr) - 1):
+        if s.startswith(substr[i:]):
+            count = 1
+            remaining_s = s[i + 1:]
+            while len(remaining_s) >= len(substr):
+                print remaining_s
+                if remaining_s.startswith(substr):
+                    count += 1
+                    remaining_s = remaining_s[len(substr):]
+                else:
+                    break
+            if substr.startswith(remaining_s):
+                count += 1
+                return count
+    return -1
--- Miscellaneous/IEEE Coding Challenge/q3tests.py
+++ Miscellaneous/IEEE Coding Challenge/q3tests.py
@@ -1,10 +1,51 @@
 from test_runner import *
 
 path = './q3'
-function_name = 'find_string_multiplier_to_contain'
+function_name = 'find_number_of_repeats'
 
 
 def all_tests(f, s):
+    empty_outer_test(f, s)
+    empty_inner_test(f, s)
+    found_test(f, s)
+    not_found_test(f, s)
+    repetition_found_test(f, s)
+    repetition_not_found_test(f, s)
+    found_timer_test(f, s)
+    not_found_timer_test(f, s)
+
+
+def empty_outer_test(f, s):
+    pass
+
+
+def empty_inner_test(f, s):
+    pass
+
+
+def found_test(f, s):
+    pass
+
+
+def not_found_test(f, s):
+    pass
+
+
+def repetition_found_test(f, s):
+    pass
+
+
+def repetition_not_found_test(f, s):
+    pass
+
+
+@timer()
+def found_timer_test(f, s):
+    pass
+
+
+@timer()
+def not_found_timer_test(f, s):
     pass
 
 
